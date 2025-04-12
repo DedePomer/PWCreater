c90bf88f225d30ed89e3f19dbba89f833dc271e9
t343s <t343s@PC-01>
2025-04-12 20:07:15 +0300

    добавил команды

---
+++ Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PWCreater.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged // Выполнятется если поменялось на ложь метод CanExecute
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object? parameter); // Ложь - команду выполнить нельзя; Истина - можно (может включать и выключать работу определённых элементов
+
+        public abstract void Execute(object? parameter); //Логика комманды
+    }
+}
---
+++ Infrastructure/Commands/LamdaComand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PWCreater.Infrastructure.Commands.Base;
+
+namespace PWCreater.Infrastructure.Commands
+{
+    internal class LamdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object?, bool> _CanExecute;
+
+        public LamdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+
+        public override void Execute(object? parameter) => _Execute(parameter);
+
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+    }
+}
