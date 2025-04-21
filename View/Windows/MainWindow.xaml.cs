d062ef9cd643003e51ca1a7c48bb0f8212b8f1c3
t343s <t343s@PC-01>
2025-04-21 22:06:04 +0300

    играюсь с визуалом

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace PWCreater
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+       public MainWindow()
         {
             InitializeComponent();
         }
