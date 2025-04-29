16a9853378148995e9e8377c58be8208854950df
LocalAdmin <LocalAdmin@LOCALADMIN-PC67>
2025-04-29 09:41:13 +0300

    создал интерфейсы

---
+++ Infrastructure/Interfaces/IStringGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWCreater.Infrastructure.Interfaces
+{
+    public interface IStringGenerator
+    {
+        public string[] GetGeneratedString (int stringCount);
+
+    }
+}
---
+++ Infrastructure/Interfaces/ISymbolCreater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWCreater.Infrastructure.Interfaces
+{
+    public interface ISymbolCreater
+    {
+        public string CreateString(string[] generatedString);
+;    }
+}
