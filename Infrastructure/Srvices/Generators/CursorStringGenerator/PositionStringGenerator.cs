f4823611aea0b1653771a8a5418b08619a804658
LocalAdmin <LocalAdmin@LOCALADMIN-PC67>
2025-05-16 11:06:34 +0300

    создалд зоны и константы для из определения

--- Infrastructure/Srvices/Generators/CursorStringGenerator/PositionStringGenerator.cs
+++ Infrastructure/Srvices/Generators/CursorStringGenerator/PositionStringGenerator.cs
@@ -11,24 +11,74 @@ namespace PWCreater.Infrastructure.Srvices.Generators.CursorStringGenerator
 {
     public class PositionStringGenerator : IStringGenerator
     {
+        /*константы для определения границ зон*/ 
+        private const float OneFourth = 0.25f;
+        private const float TwoFourths = 0.5f;
+        private const float ThreeFourths = 0.75f;
+        private const float OneThrid = 0.33f;
+        private const float TwoThrids = 0.67f;
+        private const float Unit = 1f;
+        private const float Zero = 0f;
+
+
         [DllImport("user32.dll")]
         private static extern bool GetCursorPos(out PointStruct lpPoint);
 
         [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
         private static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
 
-        private int _fistZone = 2;
-        private int _secondZone = 2;
-        private int _thirdZone = 20;
-        private int _fourthZone = 20;
-        private int _fifthZone = 10;
-        private int _sixthZone = 10;
+        private int _countDotInFistZone = 2;
+        private int _countDotInSecondZone = 2;
+        private int _countDotInThirdZone = 20;
+        private int _countDotInFourthZone = 20;
+        private int _countDotInFifthZone = 10;
+        private int _countDotInSixthZone = 10;
+
+        private RectangleZoneDataType _firstZone; /*спросить у миши как можно реализовать это по эстетичнее*/
+        private RectangleZoneDataType _secondZone;
+        private RectangleZoneDataType _thirdZone;
+        private RectangleZoneDataType _fourthZone;
+        private RectangleZoneDataType _fifthZone;
+        private RectangleZoneDataType _sixthZone;
+
+
 
 
 
         public string[] GetGeneratedString(int stringCount)
         {
             List<string> dots = new List<string>();
+            ScreenResolutionDataType screenResolution = GetScreenResolution();
+            _firstZone = new RectangleZoneDataType(
+                new DotDataType
+                { X = screenResolution.Widh * OneFourt, Y = screenResolution.Height * OneThrid },
+                new DotDataType
+                { X = screenResolution.Widh * TwoFourths, Y = screenResolution.Height * TwoThrids });
+            _secondZone = new RectangleZoneDataType(
+                new DotDataType
+                { X = screenResolution.Widh * TwoFourths, Y = screenResolution.Height * OneThrid },
+                new DotDataType
+                { X = screenResolution.Widh * ThreeFourths, Y = screenResolution.Height * TwoThrids });
+            _thirdZone = new RectangleZoneDataType(
+                new DotDataType
+                { X = screenResolution.Widh * ThreeFourths, Y = screenResolution.Height * Zero },
+                new DotDataType
+                { X = screenResolution.Widh * Unit, Y = screenResolution.Height * Zero });
+            _fourthZone = new RectangleZoneDataType(
+                new DotDataType
+                { X = screenResolution.Widh * Zero, Y = screenResolution.Height * Zero },
+                new DotDataType
+                { X = screenResolution.Widh * OneFourth, Y = screenResolution.Height * Unit });
+            _fifthZone = new RectangleZoneDataType(
+                new DotDataType
+                { X = screenResolution.Widh * OneFourth, Y = screenResolution.Height * Zero },
+                new DotDataType
+                { X = screenResolution.Widh * ThreeFourths, Y = screenResolution.Height * OneThrid });
+            _sixthZone = new RectangleZoneDataType(
+                new DotDataType
+                { X = screenResolution.Widh * OneFourth, Y = screenResolution.Height * TwoThrids },
+                new DotDataType
+                { X = screenResolution.Widh * ThreeFourths, Y = screenResolution.Height * Unit });
 
 
             throw new NotImplementedException();
