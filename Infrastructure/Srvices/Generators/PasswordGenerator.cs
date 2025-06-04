6b185e3ea3c2db8b5c3cbd4891658c31b5865905
LocalAdmin <LocalAdmin@LOCALADMIN-PC67>
2025-06-04 10:03:14 +0300

    +

--- Infrastructure/Srvices/Generators/PasswordGenerator.cs
+++ Infrastructure/Srvices/Generators/PasswordGenerator.cs
@@ -10,35 +10,22 @@ namespace PWCreater.Infrastructure.Srvices.Generators
         private const string NumberAlphabet = "1234567890";
         private const string LowerLatinLettersAlphabet = "abcdefghijklmnopqrstuvwxyz";
         private const string UpperLatinLettersAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private const string SpecialSymbolsAlphabet = "!!!!!!!@@@@@@@@#-";
+        private const string SpecialSymbolsAlphabet = "!!!!@@@@@#-";
 
         private const int CountByetsOnSha256 = 32;
+        //int index = /*averageByte*/ hashBytes[i, 0 % CountByetsOnSha256] % alphabet.Length;
 
-
-        public string GeneratePasswordFromByte(byte[,] hashBytes, int passwordLength, SymbolAlphabet symbol) 
+        public string GeneratePassword(byte[,] hashBytes, int passwordLength, SymbolAlphabet symbol) 
         {
-            RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
-            string alphabet = AlphabetCreater(symbol);
-            StringBuilder password = new StringBuilder(passwordLength);
 
-            for (int i = 0; i < passwordLength; i++)
-            {
-                int averageByte = 0;
-                for (int y = 0; y < CountByetsOnSha256; y++)
-                {
-                    averageByte += hashBytes[i, y];
-                }
-                averageByte /= CountByetsOnSha256;
-                int index = /*averageByte*/ hashBytes[i, 0 % CountByetsOnSha256] % alphabet.Length;
-                password.Append(alphabet[index]);
-
-            }
-
-            return password.ToString();
+            return ""; 
         }
 
-        private string AlphabetCreater(SymbolAlphabet symbol)
+        private string ChooseSymbols(SymbolAlphabet symbol)
         {
+            RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+            switch(randomNumberGenerator.Get)
+
             string alphabet = NumberAlphabet;
             if (symbol.LowerLatinLetters)
             {
