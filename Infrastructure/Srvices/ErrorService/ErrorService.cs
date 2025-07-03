8665766498d34b333010c67558b3bf65977d71d9
t343s <t343s@PC-01>
2025-07-03 22:41:34 +0300

    начал делать сервис для вывода ошибок

---
+++ Infrastructure/Srvices/ErrorService/ErrorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PWCreater.Infrastructure.Srvices.ErrorService.Interfaces;
+
+namespace PWCreater.Infrastructure.Srvices.ErrorService
+{
+    public class ErrorService : IErrorService
+    {
+        //создал приватный чтоб вызвать нельзя было из вне
+        private ErrorService() { }  
+
+        public ErrorService service = new ErrorService();
+
+        public event EventHandler<ErrorEventArgs> ErrorOccurred;
+
+        public void ReportError(Exception exception)
+        {
+            ErrorOccurred?.Invoke(this, new ErrorEventArgs(exception));
+        }
+    }
+}
