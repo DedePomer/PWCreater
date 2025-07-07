db9b8c1c04c05b3738f7311d8a429423c1d8d7e2
t343s <t343s@PC-01>
2025-07-07 21:59:42 +0300

    +

--- Infrastructure/Srvices/BugService/ErrorMessage.cs
+++ Infrastructure/Srvices/BugService/ErrorMessage.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PWCreater.Infrastructure.Srvices.BugService
 {
-    public class ErrorMessage: EventArgs
+    public class ErrorMessage : EventArgs
     {
         public SystemException NameOfExceptiom { get; }
-        public ErrorMessage(SystemException nameOfExceptiom) 
+        public ErrorMessage(SystemException nameOfExceptiom)
         {
             NameOfExceptiom = nameOfExceptiom;
         }
