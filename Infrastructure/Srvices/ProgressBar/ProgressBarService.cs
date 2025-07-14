e96bee5efee476736d4c8e00a878da095133aab7
LocalAdmin <LocalAdmin@LOCALADMIN-PC67>
2025-07-14 13:10:22 +0300

    +

---
+++ Infrastructure/Srvices/ProgressBar/ProgressBarService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWCreater.Infrastructure.Srvices.ProgressBar
+{
+    public class ProgressBarService
+    {
+        private ProgressBarService() { }
+
+        private ProgressBarService _service;
+        public ProgressBarService Service
+        {
+            get
+            {
+                if (_service == null)
+                {
+                    _service = new();
+                }
+                return _service;
+            }
+        }
+
+
+    }
+}
