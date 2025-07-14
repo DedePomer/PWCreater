42bca43dec209a6da51fea133ae63823724d3768
LocalAdmin <LocalAdmin@LOCALADMIN-PC67>
2025-07-14 11:17:55 +0300

    +

---
+++ ViewModel/Windows/GenerationWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PWCreater.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWCreater.ViewModel.Windows
+{
+    internal class GenerationWindowViewModel: ViewModelBase
+    {
+        private int _generationProgress;
+        public int GenerationProgress
+        {
+            get => _generationProgress;
+
+            set { Set(ref _generationProgress, value); }
+        }
+
+        public GenerationWindowViewModel()
+        {        
+        }
+    }
+}
